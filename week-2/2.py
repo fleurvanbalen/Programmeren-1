e79c35eaa8b2765913bc43942664a9a1fc90b7df
Fleur van Balen <fleur.van.balen@mac.com>
2020-09-13 01:20:43 +0200

    .

--- week-2/2.py
+++ week-2/2.py
@@ -1,40 +1,53 @@
-#benaderingen van pi (3.141592653589793238462643383279)
-def leibniz(nmax): 
-    arctan_1 = 0.0              #defineren van var
-    for n in range(nmax):       #de sommatie formule
+# pi = 3.141592653589793238462643383279...
+
+# Functie voor de benadering van pi volgens Leibniz methode
+def Leibniz(nmax): 
+    arctan_1 = 0.0          #defineren van var
+    for n in range(nmax+1): #de sommatie formule (nmax+1 omdat range(0) niet wordt uitgevoerd)
         arctan_1 += ((-1)**n)/(2*n+1)
     pi_leibniz = arctan_1 * 4   #arctan(1) geeft pi/4
     return pi_leibniz
 
-#print(leibniz(1000))       #3,140592653839794
-#print(leibniz(1000000))    #3,1415916535897743
-#print(leibniz(10000000))
+# Dit is een kleine aanpassing op de bovenstaande functie. Leibniz_2 is geen
+# functie maar een oneindige generator. Zo kan de convergentie van de reeks
+# aanschouwd worden op het standaard uitput scherm.
+def Leibniz_2():
+    n = 0
+    pi = 0
+    while True:
+        term = ((-1)**n)/(2*n+1)
+        pi += 4*term
+        n += 1
+        yield pi
+
 
 def bbp(nmax):
-    pi_bbp = 0.0                #defineren van var
-    for n in range(nmax):       #de sommatie formule
+    pi_bbp = 0.0            #defineren van var
+    for n in range(nmax+1): #de sommatie formule
         pi_bbp += (1/16**n)*((4/(8*n+1))-(2/(8*n+4))-(1/(8*n+5))-(1/(8*n+6)))
     return pi_bbp
 
 #print(bbp(1000))
 #print(bbp(10000))
+3.1415926535897913
+3.141592653589793
 
-# Met onderstaande code wordt uitgerekend bij welke nmax de benadering nauwkeurig is op 5 cijfers achter de komma (error < 1e-6) voor de Leibniz methode
-# nmax is rond de 1000000 voor 5 cijfers achter de komma
-# met `n5 in rarange(100000, 10000000, 10000)` geeft een n5 van 1010000, dus het zit tussen 1000000 en 1010000
+#3.141592653589793238462643383279 - 3.14159 = 2.6535898e-6 (fout marge voor 5 decimalen nauwkeurig)
+
+# Met onderstaande code wordt uitgerekend bij welke nmax de benadering nauwkeurig is op 5 cijfers achter de komma (error < 2.6535898e-6) voor de Leibniz methode
+# nmax is rond de 400000 voor 5 cijfers achter de komma
+# met `n5 in range(100000, 10000000, 10000)` geeft een n5 van 1010000, dus het zit tussen 1000000 en 1010000
 # range(1000000, 1010000, 100) geeft n5 = 1000100
 # range(1000000, 1000100, 1) geeft 1000001 als n5
 # Met een n van twee wordt met de Leibniz sommatie pi dus op 5 decimalen achter de komma benaderd
 
 diff = 1.0
-for n5 in range(1000000, 1010000, 1): # Een zelfgemaakte schatting wat de nmax is, aan de hand van de gemaakte berekeningen kan de range en grote van de stappen worden aangepast
-    est = leibniz(n5)
+for n5 in range(376800, 482516, 1): # Een zelfgemaakte schatting wat de nmax is, aan de hand van de gemaakte berekeningen kan de range en grote van de stappen worden aangepast
+    est = Leibniz(n5)
     diff = abs(est - 3.141592653589793238462643383279) # berekening van de fout
-    print(n5, diff, leibniz(n5))
-    if diff < 1e-6 : # Als de fout kleiner is dan 1e-6, wordt de loop onderbroken
+    print(n5, diff, est)
+    if diff < 2.6535898e-6 : # Als de fout kleiner is dan 1e-6, wordt de loop onderbroken
         break
 
-#Berekening van de fout bij bbp(10)
-diff_bbp = abs(bbp(10) - 3.141592653589793238462643383279)
+#Berekening van bbp(10) geeft 3.141592653589793
 print(bbp(10))
-print(diff_bbp) #De fout van bbp(10) is 1,77635684e-15
--- week-2/3.py
+++ week-2/3.py
@@ -1,11 +1,21 @@
+
+
 def factorise(n) :
-    for p in range(100):
-        if n % p == 0 :
-            n = n / 2
-            continue
-        else: 
-            break
-
+    number = n 
+    prime_factors = [] # aanmaken van een lege array voor de priemfactoren
+    p = 2 # priemfactor begint bij 2 want 0 geeft een delen door 9 error en 1 blijft oneindig doorgaan omdat alles oneindig door 1 kan worden gedeeld
+    while n > 1: # Als alle priemfactoren zijn gevonden is n = 1 en stopt de loop
+        if n % p == 0: # Als p een priemfactor van n is, dan geeft n modulo p = 0
+            prime_factors.append(p) # Voeg de priemfactor aan de array toe
+            n = n / p 
+            continue # Ga naar het begin van de while loop
+        p += 1 # Als er geen factoren (meer) zijn voor p, ga dan naar p + 1
+        
+    print('\nDe priemfactoren van {0} zijn:'.format(number))
+    print(*prime_factors, '\n')
+    #for i in range(len(prime_factors)): 
+    #    print(prime_factors[i], end=' ')
+    
 factorise(6936)
-
-#test
+factorise(86400000)
+factorise(7919) # priemgetal
