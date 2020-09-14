634971b2b1e2001fb1b75b60520bc20cb7fd6bcf
Fleur van Balen <fleur.van.balen@mac.com>
2020-09-14 14:39:42 +0200

    Small change to comments

--- week-2/3.py
+++ week-2/3.py
@@ -1,20 +1,17 @@
 
 
 def factorise(n) :
-    number = n 
-    prime_factors = [] # aanmaken van een lege array voor de priemfactoren
+    print('\nDe priemfactoren van {0} zijn:'.format(n))
+    prime_factors = [] # aanmaken van een lege list voor de priemfactoren
     p = 2 # priemfactor begint bij 2 want 0 geeft een delen door 9 error en 1 blijft oneindig doorgaan omdat alles oneindig door 1 kan worden gedeeld
     while n > 1: # Als alle priemfactoren zijn gevonden is n = 1 en stopt de loop
         if n % p == 0: # Als p een priemfactor van n is, dan geeft n modulo p = 0
-            prime_factors.append(p) # Voeg de priemfactor aan de array toe
+            prime_factors.append(p) # Voeg de priemfactor aan de list toe
             n = n / p 
             continue # Ga naar het begin van de while loop
-        p += 1 # Als er geen factoren (meer) zijn voor p, ga dan naar p + 1
-        
-    print('\nDe priemfactoren van {0} zijn:'.format(number))
-    print(*prime_factors, '\n')
-    #for i in range(len(prime_factors)): 
-    #    print(prime_factors[i], end=' ')
+        p += 1 # Als n geen factor p (meer) heeft, ga dan naar p + 1
+    print(*prime_factors, '\n') # Print de lijst van priemfactoren
+
     
 factorise(6936)
 factorise(86400000)
